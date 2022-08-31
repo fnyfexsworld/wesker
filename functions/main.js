d905e1c343d184d3e0acc32c69f306bb87139802
Fnyfex <87782924+fnyfexsworld@users.noreply.github.com>
2022-08-31 22:05:21 +0300

    Create main.js

---
+++ functions/main.js
@@ -0,0 +1,40 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+  
+  if (h >= 6 && h <12) {
+      document.getElementById('goodwhat').innerHTML = "günaydın!"
+  }
+  
+  if (h >= 12 && h < 18) {
+    document.getElementById('goodwhat').innerHTML = "iyi günler!"
+  }
+  
+if (h >= 18) {
+    document.getElementById('goodwhat').innerHTML = "iyi akşamlar!"
+  }
+  
+  if (h >= 0 && h < 6) {
+    document.getElementById('goodwhat').innerHTML = "iyi akşamlar!"
+  }
+}
+
+function checkTime(i) {
+  if (i < 10) { i = "0" + i }; // add zero in front of numbers < 10
+  return i;
+}
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark");
+}
