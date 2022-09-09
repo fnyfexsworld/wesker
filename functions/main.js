10d5f2adaf344e4a8740341d547c180326037b0a
Fnyfex <87782924+fnyfexsworld@users.noreply.github.com>
2022-09-09 19:39:56 +0300

    french text, mm

--- functions/main.js
+++ functions/main.js
@@ -33,8 +33,3 @@ function checkTime(i) {
   if (i < 10) { i = "0" + i }; // add zero in front of numbers < 10
   return i;
 }
-
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark");
-}
